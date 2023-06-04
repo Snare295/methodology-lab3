0571823282facdc4d21d27e33085acb11638ea17
Snare295 <vladneum@gmail.com>
2023-06-04 04:25:02 +0300

    Add matrix response and change base image

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,8 +1,18 @@
 from fastapi import APIRouter
+import numpy
 
 router = APIRouter()
 
+@router.get('/matrixes')
+def matrix() -> dict:
+    matrix_a = numpy.random.rand(10, 10)
+    matrix_b = numpy.random.rand(10, 10)
+    product = numpy.dot(matrix_a, matrix_b)
 
-@router.get('')
-def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+    dictionary = {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist()
+    }
+
+    return dictionary
